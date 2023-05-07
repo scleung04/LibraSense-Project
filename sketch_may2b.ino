4c99691295385ce326be551e861833ea2bea879a
Sean Leung <115507518+scleung04@users.noreply.github.com>
2023-05-07 11:58:39 -0400

    Arduino C++ code to work in tandem with the Python code to detect the sound level using a sound sensor

--- sketch_may2b.ino
+++ sketch_may2b.ino
@@ -3,7 +3,7 @@ void setup() {
 }
 
 void loop() {
-  int soundValue = analogRead(A2);  // read analog value from sound sensor
+  int soundValue = analogRead(A2);  // read analog value from sound sensor using the A2 pin
   Serial.println(soundValue);  // send value over serial
-  delay(100);  // wait for 100ms
+  delay(100);  // wait for 100 milliseconds
 }
