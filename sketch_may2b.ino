08011d4dc2e9747eb83ae86a17c09db3d876044a
Sean Leung <115507518+scleung04@users.noreply.github.com>
2023-05-07 12:01:02 -0400

    Arduino C++ code to detect sound level
    Work in tandem with the Python code to detect and print the sound level using a sound sensor on an Arduino Uno.

--- sketch_may2b.ino
+++ sketch_may2b.ino
@@ -3,7 +3,7 @@ void setup() {
 }
 
 void loop() {
-  int soundValue = analogRead(A2);  // read analog value from sound sensor using the A2 pin
+  int soundValue = analogRead(A2);  // read analog value from sound sensor using the A2 pin on an Arduino Uno
   Serial.println(soundValue);  // send value over serial
   delay(100);  // wait for 100 milliseconds
 }
