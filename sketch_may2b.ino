abe4a1158b4d2ad69a80e4ec3bc8d5d9f3cd27cc
Hangsihak Sin <hsin9104@conestogac.on.ca>
2023-05-12 01:17:39 -0400

    Move Sensors Folder into LibraSense_ReactNative Folder

--- sketch_may2b.ino
+++
@@ -1,9 +0,0 @@
-void setup() {
-  Serial.begin(9600);  // start serial communication
-}
-
-void loop() {
-  int soundValue = analogRead(A2);  // read analog value from sound sensor using the A2 pin on an Arduino Uno
-  Serial.println(soundValue);  // send value over serial
-  delay(100);  // wait for 100 milliseconds
-}
